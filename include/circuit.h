a09709e1101d723a01e1e83bcb00a37a62b25c3d
Chotepong Victor Tangton <65469344+S0UN@users.noreply.github.com>
2025-01-31 15:14:06 -0600

    w

--- include/circuit.h
+++ include/circuit.h
@@ -6,12 +6,13 @@
 #define MAX_DEVICES 100
 
 void makeGraph(ArrayList *devices);
- void computeTruthTable(ArrayList *devices, int numInputs);
+void computeTruthTable(ArrayList *devices, int numInputs);
 int evaluate(Device *device, int *inputs, ArrayList *devices);
-void collectAndSortDeviceIDs(ArrayList *devices, const char *type, ArrayList *result);
-void printTruthTableHeader(ArrayList *inputDevices, ArrayList *outputDevices);
-void printTruthTableRow(ArrayList *inputDevices, ArrayList *outputDevices, int *inputs, ArrayList *devices);
+int compareInt(const void *a, const void *b);
 
-int evaluateDevice(Device *device, int *inputs, ArrayList *devices);
-void buildDeviceConnections(Device *device, ArrayList *devices);
+//void collectAndSortDeviceIDs(ArrayList *devices, const char *type, ArrayList *result);
+//void printTruthTableHeader(ArrayList *inputDevices, ArrayList *outputDevices);
+//void printTruthTableRow(ArrayList *inputDevices, ArrayList *outputDevices, int *inputs, ArrayList *devices);
+//int evaluateDevice(Device *device, int *inputs, ArrayList *devices);
+//void buildDeviceConnections(Device *device, ArrayList *devices);
 #endif 
